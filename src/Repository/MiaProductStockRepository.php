f0baaaba2284b704d5a1c8885d43c608d63845c9
Matias Camiletti <matias.camiletti@gmail.com>
2021-09-17 11:00:47 -0300

    Add repositories.

---
+++ src/Repository/MiaProductStockRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mia\Market\Repository;
+
+use \Illuminate\Database\Capsule\Manager as DB;
+
+/**
+ * Description of MiaProductStockRepository
+ *
+ * @author matiascamiletti
+ */
+class MiaProductStockRepository 
+{
+    /**
+     * 
+     * @param \Mia\Database\Query\Configure $configure
+     * @return \Illuminate\Pagination\Paginator
+     */
+    public static function fetchByConfigure(\Mia\Database\Query\Configure $configure)
+    {
+        $query = \Mia\Market\Model\MiaProductStock::select('mia_product_stock.*');
+        
+        if(!$configure->hasOrder()){
+            $query->orderByRaw('id DESC');
+        }
+        $search = $configure->getSearch();
+        if($search != ''){
+            //$values = $search . '|' . implode('|', explode(' ', $search));
+            //$query->whereRaw('(firstname REGEXP ? OR lastname REGEXP ? OR email REGEXP ?)', [$values, $values, $values]);
+        }
+        
+        // Procesar parametros
+        $configure->run($query);
+
+        return $query->paginate($configure->getLimit(), ['*'], 'page', $configure->getPage());
+    }
+}
